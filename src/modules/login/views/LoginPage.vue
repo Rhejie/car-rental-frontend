7a03e1059fbbce7ffcf38048f3cd0b7625889e56
unknown <rhejie.aranezg210904@gmail.com>
2022-12-05 15:09:27 +0800

    update

--- src/modules/login/views/LoginPage.vue
+++ src/modules/login/views/LoginPage.vue
@@ -59,7 +59,7 @@ const handleClickLogIn = async () => {
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                <img class="mx-auto w-80" src="../../../assets/APCLogo.jpg"
                     alt="Your Company" />
                 <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
             </div>
