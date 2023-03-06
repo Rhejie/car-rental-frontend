2b023b53381130f698138174464405b2015b390b
unknown <rhejie.aranezg210904@gmail.com>
2023-03-06 13:23:44 +0800

    update

--- src/modules/user/modal/BookingModal.vue
+++ src/modules/user/modal/BookingModal.vue
@@ -366,7 +366,7 @@ const loadAllBookedDates = async () => {
                                                                             class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                                                                             {{
                                                                                     vehicle.fuel_consumption
-                                                                            }} letter(s) per km</dd>
+                                                                            }} liter(s) per km</dd>
                                                                     </div>
                                                                     <div
                                                                         class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
