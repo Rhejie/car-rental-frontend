6f8cbd0be2908a819bae802789f3636f42a37e1d
unknown <rhejie.aranezg210904@gmail.com>
2023-03-06 13:25:41 +0800

    update

--- src/modules/user/views/VehicleDetails.vue
+++ src/modules/user/views/VehicleDetails.vue
@@ -132,7 +132,7 @@ onMounted(async () => {
                                             <dt class="text-sm font-medium text-gray-500">Fuel Consumption</dt>
                                             <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{{
                                                     vehicle.fuel_consumption
-                                            }} letter(s) per km</dd>
+                                            }} liter(s) per km</dd>
                                         </div>
                                         <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                             <dt class="text-sm font-medium text-gray-500">Color</dt>
